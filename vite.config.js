07dc6a4a149d0bbbd31af3418bb31d239f7ee82e
notgro03 <geroguerriero03@gmail.com>
2024-11-26 13:17:45 -0300

    LINKS

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   build: {
@@ -13,6 +14,9 @@ export default defineConfig({
         'contacto': 'pages/contacto.html',
         'tutoriales': 'pages/tutoriales.html',
         'telemandos': 'pages/telemandos.html',
+        'llaves': 'pages/llaves.html',
+        'carcasas': 'pages/carcasas.html',
+        'accesorios': 'pages/accesorios.html',
         'blog': 'pages/blog.html',
         'faq': 'pages/faq.html',
         'privacidad': 'pages/privacidad.html',
@@ -23,5 +27,21 @@ export default defineConfig({
   base: '',
   server: {
     open: true
-  }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src')
+    }
+  },
+  plugins: [{
+    name: 'rewrite-middleware',
+    configureServer(server) {
+      server.middlewares.use((req, res, next) => {
+        if (req.url.endsWith('.html')) {
+          req.url = req.url.slice(0, -5)
+        }
+        next()
+      })
+    }
+  }]
 })
