83a5e29cd71d6388a94a68236b03f61802d39034
notgro03 <geroguerriero03@gmail.com>
2024-12-05 17:58:34 -0300

    02

--- vite.config.js
+++ vite.config.js
@@ -27,8 +27,8 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
-      '@admin': resolve(__dirname, 'src/admin')
+      '@': resolve(__dirname, './src'),
+      '@admin': resolve(__dirname, './src/admin')
     }
   },
   server: {
